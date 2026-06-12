c4446c9311407436035bc5ff2c84b1fc3eafe7a9
todaynow0101-sys <todaynow0101@gmail.com>
2026-06-12 16:18:46 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='pay-eng-v1';
+const ASSETS=['/payroll-english/','/payroll-english/index.html','/payroll-english/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
